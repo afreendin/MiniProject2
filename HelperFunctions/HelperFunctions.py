61653ce2c1b6428c9d68ba1f2f0c242e8223ba84
afreendin <afu2@njit.edu>
2020-03-29 11:07:51 -0400

    Created Unit Test and Helper Functions

--- HelperFunctions/HelperFunctions.py
+++ HelperFunctions/HelperFunctions.py
@@ -1,3 +1,9 @@
+import statistics as stat
+from scipy import stats
+from scipy.stats.stats import pearsonr
+import numpy as np
+
+
 class HelperFunctions:
 
     result = 0
@@ -29,3 +35,37 @@ class HelperFunctions:
         self.result = float(a)**(1/2.0)
         return self.result
 
+    # Library functions
+
+    def mean(self, data):
+        self.result = stat.mean(data)
+        return "{0:.2f}".format(round(self.result, 2))
+
+    def median(self, data):
+        self.result = stat.median(data)
+        return "{0:.2f}".format(round(self.result, 2))
+
+    def mode(self, data):
+        self.result = stat.mode(data)
+        return self.result
+
+    def variance(self, data):
+        self.result = stat.variance(data)
+        return "{0:.2f}".format(round(self.result, 2))
+
+    def standard_deviation(self, data):
+        self.result = stat.stdev(data)
+        return "{0:.2f}".format(round(self.result, 2))
+
+    def skewness(self, data):
+        skew = 3*(float(self.mean(data)) - float(self.median(data)))/float(self.standard_deviation(data))
+        return "{0:.2f}".format(round(skew, 2))
+
+    def sample_correlation(self, datax, datay):
+        corr = pearsonr(datax, datay)
+        return  corr
+
+    def z_score(self, datax):
+        zscore = stats.zscore(datax)
+        zscore = ["%.2f"%item for item in zscore]
+        return [float(k) for k in zscore]
