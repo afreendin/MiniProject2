da8232710bb13e4114a36183579616bb6c9cd866
afreendin <afu2@njit.edu>
2020-03-30 12:57:00 -0400

    changed test_calculator file and helper function file

--- HelperFunctions/HelperFunctions.py
+++ HelperFunctions/HelperFunctions.py
@@ -4,68 +4,103 @@ from scipy.stats.stats import pearsonr
 import numpy as np
 
 
-class HelperFunctions:
+# class HelperFunctions:
+#
+#     result = 0.0
 
-    result = 0.0
+# def __init__(self):
+#     pass
 
-    def __init__(self):
-        pass
 
-    def add(self, a, b):
-        self.result = float(a) + float(b)
-        return self.result
+def add(a, b):
+    result = float(a) + float(b)
+    return result
 
-    def subtract(self, a, b):
-        self.result = float(a) - float(b)
-        return self.result
 
-    def multiply(self, a, b):
-        self.result = float(a) * float(b)
-        return self.result
+def subtract(a, b):
+    result = float(a) - float(b)
+    return result
 
-    def division(self, a, b):
-        self.result = float(a) / float(b)
-        return self.result
 
-    def square(self, a):
-        self.result = float(a)**2;
-        return self.result
+def multiply(a, b):
+    result = float(a) * float(b)
+    return result
 
-    def square_root(self, a):
-        self.result = float(a)**(1/2.0)
-        return self.result
 
-    # Library functions
+def division(a, b):
+    result = float(a) / float(b)
+    return result
 
-    def mean(self, data):
-        self.result = stat.mean(data)
-        return "{0:.2f}".format(round(self.result, 2))
 
-    def median(self, data):
-        self.result = stat.median(data)
-        return "{0:.2f}".format(round(self.result, 2))
+def square(a):
+    result = float(a)**2
+    return result
 
-    def mode(self, data):
-        self.result = stat.mode(data)
-        return self.result
 
-    def variance(self, data):
-        self.result = stat.variance(data)
-        return "{0:.2f}".format(round(self.result, 2))
+def square_root(a):
+    result = float(a)**(1/2.0)
+    return result
 
-    def standard_deviation(self, data):
-        self.result = stat.stdev(data)
-        return "{0:.2f}".format(round(self.result, 2))
 
-    def skewness(self, data):
-        skew = 3*(float(self.mean(data)) - float(self.median(data)))/float(self.standard_deviation(data))
-        return "{0:.2f}".format(round(skew, 2))
+# def add(self, a, b):
+#     self.result = float(a) + float(b)
+#     return self.result
+#
+# def subtract(self, a, b):
+#     self.result = float(a) - float(b)
+#     return self.result
+#
+# def multiply(self, a, b):
+#     self.result = float(a) * float(b)
+#     return self.result
+#
+# def division(self, a, b):
+#     self.result = float(a) / float(b)
+#     return self.result
+#
+# def square(self, a):
+#     self.result = float(a)**2;
+#     return self.result
+#
+# def square_root(self, a):
+#     self.result = float(a)**(1/2.0)
+#     return self.result
 
-    def sample_correlation(self, datax, datay):
-        corr = pearsonr(datax, datay)
-        return  corr
 
-    def z_score(self, datax):
-        zscore = stats.zscore(datax)
-        zscore = ["%.2f"%item for item in zscore]
-        return [float(k) for k in zscore]
+
+# Library functions
+#
+# def mean(self, data):
+#     self.result = stat.mean(data)
+#     return "{0:.2f}".format(round(self.result, 2))
+#
+# def median(self, data):
+#     self.result = stat.median(data)
+#     return "{0:.2f}".format(round(self.result, 2))
+#
+# def mode(self, data):
+#     self.result = stat.mode(data)
+#     return self.result
+#
+# def variance(self, data):
+#     self.result = stat.variance(data)
+#     return "{0:.2f}".format(round(self.result, 2))
+#
+# def standard_deviation(self, data):
+#     self.result = stat.stdev(data)
+#     return "{0:.2f}".format(round(self.result, 2))
+#
+# def skewness(self, data):
+#     skew = 3*(float(self.mean(data)) - float(self.median(data)))/float(self.standard_deviation(data))
+#     return "{0:.2f}".format(round(skew, 2))
+#
+# def sample_correlation(self, datax, datay):
+#     corr = pearsonr(datax, datay)
+#     return  corr
+#
+# def z_score(self, datax):
+#     zscore = stats.zscore(datax)
+#     zscore = ["%.2f"%item for item in zscore]
+#     return [float(k) for k in zscore]
+#
+#
--- UnitTest/test_Calculator.py
+++ UnitTest/test_Calculator.py
@@ -1,11 +1,20 @@
-from HelperFunctions import HelperFunctions
+import sys
+
+sys.path.append('../HelperFunctions')
+from HelperFunctions.HelperFunctions import add
+from HelperFunctions.HelperFunctions import subtract
+from HelperFunctions.HelperFunctions import multiply
+from HelperFunctions.HelperFunctions import division
+from HelperFunctions.HelperFunctions import square
+from HelperFunctions.HelperFunctions import square_root
 import unittest
 import csv
-from Calculator.Addition import addition
-from Calculator.Subtraction import subtraction
-from Calculator.Division import division
-from Calculator.Multiplication import multiplication
-from Calculator.Sqrt import root
+
+# from Calculator.Addition import addition
+# from Calculator.Subtraction import subtraction
+# from Calculator.Division import division
+# from Calculator.Multiplication import multiplication
+# from Calculator.Sqrt import root
 
 
 class MyTestCase(unittest.TestCase):
@@ -15,43 +24,44 @@ class MyTestCase(unittest.TestCase):
         pass
 
     def test_add(self):
-        self.assertEqual(self.addition(10,20), 30.0)
+        self.assertEqual(add(10,20), 30.0)
 
     def test_subtract(self):
-       self.assertEqual(self.subtraction(30,10), 20.0)
+       self.assertEqual(subtract(30,10), 20.0)
 
     def test_multiply(self):
-       self.assertEqual(self.multiplication(3,2), 6.0)
+       self.assertEqual(multiply(3,2), 6.0)
 
     def test_division(self):
-        self.assertEqual(self.division(10,2), 5.0)
+        self.assertEqual(division(10,2), 5.0)
 
     def test_square(self):
-        self.assertEqual(self.square(3), 9.0)
+        self.assertEqual(square(3), 9.0)
 
     def test_square_root(self):
-        self.assertEqual(self.sqrt(9), 3.0)
+        self.assertEqual(square_root(9), 3.0)
 
     # def test_instantiate_calculator(self):
     #     self.assertIsInstance(self.calc, HelperFunctions)
 
+
     # def test_add(self):
-    #     self.assertEqual(self.calc.add(10,20), 30.0)
+    #     self.assertEqual(self.addition(10,20), 30.0)
     #
     # def test_subtract(self):
-    #    self.assertEqual(self.calc.subtract(30,10), 20.0)
+    #    self.assertEqual(self.subtraction(30,10), 20.0)
     #
     # def test_multiply(self):
-    #    self.assertEqual(self.calc.multiply(3,2), 6.0)
+    #    self.assertEqual(self.multiplication(3,2), 6.0)
     #
     # def test_division(self):
-    #     self.assertEqual(self.calc.division(10,2), 5.0)
+    #     self.assertEqual(self.division(10,2), 5.0)
     #
     # def test_square(self):
-    #     self.assertEqual(self.calc.square(3), 9.0)
+    #     self.assertEqual(self.square(3), 9.0)
     #
     # def test_square_root(self):
-    #     self.assertEqual(self.calc.square_root(9), 3.0)
+    #     self.assertEqual(self.sqrt(9), 3.0)
 
 
 if __name__ == '__main__':
